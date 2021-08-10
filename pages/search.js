eb258504869fb567b6ff914568c6b9be7f4a8b2b
Indu Chandana <indu.99.ic@gmail.com>
2021-08-10 21:20:33 +0530

    5 comment

--- pages/search.js
+++ pages/search.js
@@ -10,12 +10,12 @@ function Search() {
                 <section>
                     <p className="text-xs">Hello</p>
                     <h1 className="text-3xl font-semibold mt-2 mb-6">Stay in Mars</h1>
-                    <div className="flex">
-                        <p className="bg-gray-100 rounded-full px-4 py-2 border cursor-pointer hover:shadow-lg active:scale-95">Cancellation Flexibility</p>
-                        <p className="bg-gray-100 rounded-full px-4 py-2 border cursor-pointer hover:shadow-lg active:scale-95">Type of place</p>
-                        <p className="bg-gray-100 rounded-full px-4 py-2 border cursor-pointer hover:shadow-lg active:scale-95">Price</p>
-                        <p className="bg-gray-100 rounded-full px-4 py-2 border cursor-pointer hover:shadow-lg active:scale-95">Rooms and Beds</p>
-                        <p className="bg-gray-100 rounded-full px-4 py-2 border cursor-pointer hover:shadow-lg active:scale-95">More filters</p>
+                    <div className="">
+                        <p className="button">Cancellation Flexibility</p>
+                        <p className="button">Type of place</p>
+                        <p className=" button">Price</p>
+                        <p className=" button">Rooms and Beds</p>
+                        <p className=" button">More filters</p>
                     </div>
                 </section>
             </main>
